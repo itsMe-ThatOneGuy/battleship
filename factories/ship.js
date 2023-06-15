fd071fb7c3a79e96680a31f2742977346d85ddf5
Matthew Smart <matthewsmart13@gmail.com>
2023-06-15 15:21:53 -0500

    Added methods to ship class

--- factories/ship.js
+++ factories/ship.js
@@ -1,6 +1,22 @@
 export class Ship {
-    constructor() { }
+    constructor(length) {
+        this.length = length;
+        this.hits = 0;
+    }
+
     test() {
         return 'hello world';
+    }
+
+    hit() {
+        return this.hits++;
+    }
+
+    getHits() {
+        return this.hits;
+    }
+
+    isSunk() {
+        return this.hits === this.length ? true : false;
     }
 }
