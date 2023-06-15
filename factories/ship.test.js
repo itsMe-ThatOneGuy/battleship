878191be844f1793e163fc5c612980158bce7931
Matthew Smart <matthewsmart13@gmail.com>
2023-06-15 15:22:44 -0500

    Tests for ship class

--- factories/ship.test.js
+++ factories/ship.test.js
@@ -1,8 +1,22 @@
 import { Ship } from './ship.js';
 
 describe('Ship', () => {
+	let testShip;
+	beforeAll(() => {
+		testShip = new Ship(1);
+		return testShip;
+	});
+
 	test('Ship Method test returns hello world', () => {
-		const testShip = new Ship();
 		expect(testShip.test()).toBe('hello world');
+	});
+
+	test('Ship receives hits', () => {
+		testShip.hit(1);
+		expect(testShip.getHits()).toBe(1);
+	});
+
+	test('Ship is sunk', () => {
+		expect(testShip.isSunk()).toBeTruthy();
 	});
 });
