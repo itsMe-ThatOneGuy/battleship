3d1e12c129b1032c6c4b14ec09a525bfb060440f
Matthew Smart <matthewsmart13@gmail.com>
2023-08-29 12:45:37 -0500

    Created the placeShip method

--- factories/gameboard.js
+++ factories/gameboard.js
@@ -16,6 +16,27 @@ class Gameboard {
 			.fill(new GameboardSpace())
 			.map((_) => new Array(10).fill(new GameboardSpace()));
 	}
-}
 
+	placeShip(column, row, direction, ship) {
+		if (direction === 'vertical') {
+			if (row + ship.length > 10) {
+				return 'Ship too long';
+			} else {
+				for (let i = 0; i < ship.length; i++) {
+					this.gameboard[column][i].ship = ship;
+				}
+			}
+		}
+
+		if (direction === 'horizontal') {
+			if (column + ship.length > 10) {
+				return 'Ship too long';
+			} else {
+				for (let i = 0; i < ship.length; i++) {
+					this.gameboard[i][row].ship = ship;
+				}
+			}
+		}
+	}
+}
 export default Gameboard;
