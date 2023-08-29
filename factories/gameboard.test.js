d6756de5e38e59b8b15a39ebf8e3447a2e8c69ea
Matthew Smart <matthewsmart13@gmail.com>
2023-08-29 01:22:44 -0500

    Basic tests for Gameboard

--- factories/gameboard.test.js
+++ factories/gameboard.test.js
@@ -1,6 +1,16 @@
-import { gameboard } from 'gameboard.js';
+import Gameboard from './gameboard';
 
 describe('gameboard', () => {
+	let testGameboard;
+	beforeAll(() => {
+		testGameboard = new Gameboard();
+		return testGameboard;
+	});
+
+	test('Gameboard is created as a 10x10 grid', () => {
+		expect(testGameboard.gameboard.length).toBe(10);
+	});
+
 	test('Ship should be placed a provided space', () => {});
 
 	test('Board receives attacks', () => {});
