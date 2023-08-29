3680a7f57ad02a17e972577af596e1296529c1b1
Matthew Smart <matthewsmart13@gmail.com>
2023-08-29 13:33:39 -0500

    Fixed bad tests

--- factories/gameboard.test.js
+++ factories/gameboard.test.js
@@ -2,9 +2,8 @@ import Gameboard from './gameboard';
 
 describe('gameboard', () => {
 	let testGameboard;
-	beforeAll(() => {
+	beforeEach(() => {
 		testGameboard = new Gameboard();
-		return testGameboard;
 	});
 
 	test('Gameboard is created with 10 columns', () => {
@@ -16,19 +15,34 @@ describe('gameboard', () => {
 	});
 
 	test('Ship is placed horizontally starting at provided space', () => {
-		expect(testGameboard.placeShip(0, 0, 'horizontal', { length: 2 }));
+		testGameboard.placeShip(0, 0, 'horizontal', {
+			length: 2,
+			name: 'testShip',
+		});
+		expect(
+			testGameboard.gameboard[0][0].ship.name &&
+				testGameboard.gameboard[1][0].ship.name
+		).toBe('testShip');
 	});
 
 	test('Ship is placed vertically starting at provided space', () => {
-		expect(testGameboard.placeShip(0, 0, 'vertical', { length: 2 }));
+		testGameboard.placeShip(0, 0, 'vertical', { length: 2, name: 'testShip' });
+		expect(
+			testGameboard.gameboard[0][0].ship.name &&
+				testGameboard.gameboard[0][1].ship.name
+		).toBe('testShip');
 	});
 
 	test('Ship is not placed horizontally if ship would be out of bounds', () => {
-		expect(testGameboard.placeShip(9, 0, 'horizontal', { length: 2 }));
+		expect(testGameboard.placeShip(9, 0, 'horizontal', { length: 2 })).toBe(
+			'Ship too long'
+		);
 	});
 
 	test('Ship is not placed vertically if ship would be out of bounds', () => {
-		expect(testGameboard.placeShip(0, 9, 'horizontal', { length: 2 }));
+		expect(testGameboard.placeShip(0, 9, 'vertical', { length: 2 })).toBe(
+			'Ship too long'
+		);
 	});
 
 	test('Board receives attacks', () => {});
