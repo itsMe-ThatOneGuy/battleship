94b50ab389e4a286c86c7f0de43e8c8baf0b9e1b
Matthew Smart <matthewsmart13@gmail.com>
2023-08-28 23:42:20 -0500

    Starting to work on gameboard

---
+++ factories/gameboard.test.js
@@ -0,0 +1,11 @@
+import { gameboard } from 'gameboard.js';
+
+describe('gameboard', () => {
+	test('Ship should be placed a provided space', () => {});
+
+	test('Board receives attacks', () => {});
+
+	test('Board keeps track of missed shots', () => {});
+
+	test('Board reports if ship has sunk', () => {});
+});
