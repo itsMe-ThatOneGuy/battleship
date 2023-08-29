ce2228d7a9ca3ee09c39501b55e7c56a93ec855a
Matthew Smart <matthewsmart13@gmail.com>
2023-08-29 12:44:53 -0500

    Added tests for placement validation

--- factories/gameboard.test.js
+++ factories/gameboard.test.js
@@ -15,7 +15,21 @@ describe('gameboard', () => {
 		expect(testGameboard.gameboard[0].length).toBe(10);
 	});
 
-	test('Ship should be placed a provided space', () => {});
+	test('Ship is placed horizontally starting at provided space', () => {
+		expect(testGameboard.placeShip(0, 0, 'horizontal', { length: 2 }));
+	});
+
+	test('Ship is placed vertically starting at provided space', () => {
+		expect(testGameboard.placeShip(0, 0, 'vertical', { length: 2 }));
+	});
+
+	test('Ship is not placed horizontally if ship would be out of bounds', () => {
+		expect(testGameboard.placeShip(9, 0, 'horizontal', { length: 2 }));
+	});
+
+	test('Ship is not placed vertically if ship would be out of bounds', () => {
+		expect(testGameboard.placeShip(0, 9, 'horizontal', { length: 2 }));
+	});
 
 	test('Board receives attacks', () => {});
 
